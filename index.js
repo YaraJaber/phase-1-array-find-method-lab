ebb591d9ea1cb0e7d08500a410facf47ae82d201
YaraJaber <145011720+YaraJaber@users.noreply.github.com>
2023-10-04 20:56:15 +0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// code your solution here
+function superbowlWin(record) {
+    const winningYear = record.find(entry => entry.result === "W");
+    
+    if (winningYear) {
+      return winningYear.year;
+    } else {
+      return undefined;
+    }
+  }
